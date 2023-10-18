f16eb1248fe9a10dac23da189bfc98a7042eb23a
manurevelles <101949955+manurevelles@users.noreply.github.com>
2023-10-18 01:14:22 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+let player = {
+    name: "Manu",
+    chips: 150
+}
+
 let cards = []
 let sum = 0
 let hasBlackJack = false
@@ -6,24 +11,28 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + " " + player.chips + "€"
 
 function getRandomCard() {
-    let randomNumer = Math.floor( Math.random()*13 ) + 1
-    if (randomNumer > 10) {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
         return 10
-    } else if (randomNumer === 1) {
+    } else if (randomNumber === 1) {
         return 11
     } else {
-        return randomNumer
+        return randomNumber
     }
 }
 
 function startGame() {
     isAlive = true
-    let firstCard = getRandomCard()
-    let secondCard = getRandomCard()
-    cards = [firstCard, secondCard]
-    sum = firstCard + secondCard
+    for (let i = 0; i < 2; i++) {
+        let card = getRandomCard()
+        cards.push(card)
+        sum += card
+    }
     renderGame()
 }
 
@@ -48,8 +57,10 @@ function renderGame() {
 
 
 function newCard() {
-    let card = getRandomCard()
-    sum += card
-    cards.push(card)
-    renderGame()
+    if (isAlive && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()        
+    }
 }
